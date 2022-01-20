59900ed14080b87f306d59bfa53bc4799546403b
Egor <egkvih@mail.ru>
2022-01-20 15:58:02 +0300

    hw2

---
+++ Homework-2/localhost/app/Http/Controllers/NewsCategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsCategoryController extends Controller
+{
+    public function show()
+    {
+        $response = "<div> 
+                        <span> So i make </span> 
+                     </div>";
+                     
+        return $response;
+    }
+}
---
+++ Homework-2/localhost/app/Http/Controllers/TargetNewsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TargetNewsController extends Controller
+{
+    public function show()
+    {
+        $response = "<div> 
+                        <span> Just this s#%*&t! </span> 
+                     </div>";
+                     
+        return $response;
+    }
+}
